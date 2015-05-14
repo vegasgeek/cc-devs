f71e827fd29026ffaf75e22d3838983949f4cff7
John Hawkins <john@9seeds.com>
2015-05-14 11:45:09 -0700

    Added field to settings page

--- ccdevs.php
+++ ccdevs.php
@@ -6,7 +6,7 @@
  * Author:            John Hawkins & Todd Huish
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       booktrope
+ * Text Domain:       ccdevs
  */
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
--- includes/functions.php
+++ includes/functions.php
@@ -7,3 +7,35 @@
  * 4. Append a link to emails that would allow a dev to click and unsubscribe from the list
  * - - The link should include the token. If the token matches a transient, we remove the dev from the CC field 
  */
+
+
+add_action( 'admin_init', 'ccd_add_settings_section' );
+function ccd_add_settings_section() {
+    add_settings_section(  
+        'ccd_settings_section', // Section ID 
+        'CC Devs', // Section Title
+        'ccd_section_options_callback', // Callback
+        'general' // What Page?  This makes the section show up on the General Settings Page
+    );
+
+    add_settings_field( // Option 1
+        'ccdev_list', // Option ID
+        'Dev Emails', // Label
+        'ccdev_list_callback', // !important - This is where the args go!
+        'general', // Page it will be displayed (General Settings)
+        'ccd_settings_section', // Name of our section
+        array( // The $args
+            'ccdev_list' // Should match Option ID
+        )  
+    ); 
+  register_setting( 'general','ccdev_list', 'esc_attr' );
+}
+
+function ccd_section_options_callback() { // Section Callback
+    echo '<p>'. __( 'Add a comma separated list of email addresses to receive copies of emails sent to the site admin.', 'ccdevs' ) .'</p>';  
+}
+
+function ccdev_list_callback($args) {  // Textbox Callback
+    $option = get_option($args[0]);
+    echo '<input type="text" id="'. $args[0] .'" name="'. $args[0] .'" value="' . $option . '" />';
+}
