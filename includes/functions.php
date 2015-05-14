1f54ab0c39cac8ca38f7537d89ae1f1b6c4afaf6
John Hawkins <john@9seeds.com>
2015-05-14 11:30:42 -0700

    Added basic plugin roadmap

--- includes/functions.php
+++ includes/functions.php
@@ -1,1 +1,9 @@
 <?php
+/**
+ * Items that need to be built:
+ * 1. Add field to general settings page to allow for comma separated list of email address
+ * 2. If emails exist in field, add them to CC field for any emails going to admin
+ * 3. Store a transient token for 3 days to allow for CC'd person to be allowed to alter that field, even if not logged in
+ * 4. Append a link to emails that would allow a dev to click and unsubscribe from the list
+ * - - The link should include the token. If the token matches a transient, we remove the dev from the CC field 
+ */
